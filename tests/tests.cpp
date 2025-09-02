ffc4c618857d3bbe8b2d163024307506963ddf6c
proxymo75015 <francois.simon75@gmail.com>
2025-09-02 13:44:36 +0200

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,34 +5,35 @@
 
 namespace fs = std::filesystem;
 
-// Déclarations des fonctions testées
-fs::path make_long_path(const fs::path& path);
-void log_error(const fs::path& srcPath, const std::string& message);
+using robot::log_error;
+#ifdef _WIN32
+using robot::make_long_path;
+#endif
 
 TEST_CASE("Path handling") {
+#ifdef _WIN32
     SECTION("Valid local path") {
         fs::path path = "test/file.rbt";
-        auto result = make_long_path(path);
-        REQUIRE(fs::absolute(path).string() == result.string());
+        fs::path result = make_long_path(path.wstring());
+        REQUIRE(fs::absolute(path) == result);
     }
 
-#if defined(_WIN32)
     SECTION("Long path handling") {
         fs::path longPath = std::string(300, 'a') + "\\file.rbt";
-        auto result = make_long_path(longPath);
-        REQUIRE(result.wstring().starts_with(L"\\\\?\\"));
+        auto result = make_long_path(longPath.wstring());
+        REQUIRE(result.starts_with(L"\\\\?\\"));
     }
 
     SECTION("UNC path handling") {
         fs::path uncPath = "\\\\server\\share\\file.rbt";
-        auto result = make_long_path(uncPath);
-        REQUIRE(result.wstring() == uncPath.wstring());
+        auto result = make_long_path(uncPath.wstring());
+        REQUIRE(result == uncPath.wstring());
     }
 
     SECTION("Excessive UNC path length") {
         fs::path uncPath = "\\\\server\\share\\" + std::string(32768, 'a') + "\\file.rbt";
-        auto result = make_long_path(uncPath);
-        REQUIRE(result.wstring() == uncPath.wstring());
+        auto result = make_long_path(uncPath.wstring());
+        REQUIRE(result == uncPath.wstring());
         // log_error est appelé, mais nous ne testons pas la sortie ici
     }
 #endif
