8559764be092fc8e923b7d39b3539b5b3b601c9a
proxymo75015 <francois.simon75@gmail.com>
2025-09-10 15:26:45 +0200

    Update png_unicode_filename.cpp

--- tests/png_unicode_filename.cpp
+++ tests/png_unicode_filename.cpp
@@ -11,8 +11,8 @@ TEST_CASE("PNG export supports non-ASCII filenames") {
     fs::path filename = L"éxport_test.png";
     std::vector<unsigned char> pixel = {255, 0, 0, 255};
     auto longPath = robot::make_long_path(filename.wstring());
-    auto utf8 = fs::path{longPath}.string();
-    REQUIRE(stbi_write_png(utf8.c_str(), 1, 1, 4, pixel.data(), 4) != 0);
+    auto utf8 = fs::path{longPath}.u8string();
+    REQUIRE(stbi_write_png(reinterpret_cast<const char *>(utf8.c_str()), 1, 1, 4, pixel.data(), 4) != 0);
     REQUIRE(fs::exists(filename));
     fs::remove(filename);
 }
