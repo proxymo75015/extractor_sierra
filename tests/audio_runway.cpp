1100241e8207c2fe8bf0dd553002f1ffbafafa7f
proxymo75015 <francois.simon75@gmail.com>
2025-09-11 15:44:15 +0200

    Update audio_runway.cpp

--- tests/audio_runway.cpp
+++ tests/audio_runway.cpp
@@ -92,5 +92,5 @@ TEST_CASE("Audio block with runway is handled") {
     out.close();
 
     robot::RobotExtractor extractor(input, outDir, true);
-    REQUIRE_THROWS_AS(extractor.extract(), std::runtime_error);
+    REQUIRE_NOTHROW(extractor.extract());
 }
