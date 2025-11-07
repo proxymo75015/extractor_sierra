7dfb1221670e0795ea380dc7c0634e66df38f4c5
SIMON <francois.simon75@gmail.com>
2025-11-07 15:10:30 +0100

    Update robot_extractor.cpp

--- src/robot_extractor.cpp
+++ src/robot_extractor.cpp
@@ -147,7 +147,7 @@ void RobotExtractor::readHeader() {
 
   m_postHeaderPos = m_fp.tellg();
   
-  if (m_version < 5 || m_version > 6) {
+  if (m_version < 4 || m_version > 6) {
     throw std::runtime_error("Version Robot non supportée: " +
                              std::to_string(m_version));
   }
@@ -180,7 +180,7 @@ void RobotExtractor::parseHeaderFields(bool bigEndian) {
   }
 
   m_version = read_scalar<uint16_t>(m_fp, m_bigEndian);
-  if (m_version < 5 || m_version > 6) {
+  if (m_version < 4 || m_version > 6) {
     throw std::runtime_error("Version Robot non supportée: " +
                              std::to_string(m_version));
   }
