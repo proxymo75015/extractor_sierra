cb407cafac4c6024e3e5a0180dfe77ce8b8cb52b
SIMON <francois.simon75@gmail.com>
2025-11-13 15:12:06 +0000

    suppression 1

--- tests/png_unicode_filename.cpp
+++
@@ -1,4 +0,0 @@
-// Test spécifique Windows retiré du jeu de tests principal car il
-// vérifie le comportement d'une bibliothèque tierce (stb_image_write)
-// plutôt que la logique de ce projet. Conserver dans l'historique si besoin.
-// (Intentionnellement vide.)
--- tests/unicode_long_path.cpp
+++
@@ -1,3 +0,0 @@
-// Test spécifique Windows retiré : vérifie uniquement des aspects de chemin
-// Unicode et la lib stb_image_write. Non essentiel au cœur de l'extraction.
-// (Intentionnellement vide.)
